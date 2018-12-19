edf8adb3892a757d32ab91e60f41d0424c75e72d
Tara Johnson <tara@madface.com>
2018-12-18 20:25:19 -0800

    Project model and controller in progress.

--- src/main/java/org/seattlevoluntech/controllers/HomeController.java
+++ src/main/java/org/seattlevoluntech/controllers/HomeController.java
@@ -1,12 +1,23 @@
 package org.seattlevoluntech.controllers;
 
+import org.seattlevoluntech.models.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class HomeController {
     @RequestMapping(value = "/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/projects")
+    public List<Project> projects() {
+        Project blah = new Project("Richard's Project");
+        return Arrays.asList(blah);
     }
 }
---
+++ src/main/java/org/seattlevoluntech/models/Project.java
@@ -0,0 +1,9 @@
+package org.seattlevoluntech.models;
+
+public class Project {
+    private String name;
+
+    public Project(String name) {
+        this.name = name;
+    }
+}
