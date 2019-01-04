d997c6a0a4d11b93f96f799faca6689267966d5a
Ming Ho <hkboi108@hotmail.com>
2019-01-03 16:00:02 -0800

    Added new nav links

--- front-end/src/components/nav-ui/nav-ui.js
+++ front-end/src/components/nav-ui/nav-ui.js
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as routes from '../../routes';
 import './nav-ui.scss';
-import voluntechLogo from '../../../assets/voluntechLogo.JPG';
+import voluntechLogo from '../../../assets/voluntechLogo.jpg';
 
 class NavUi extends React.Component {
   // CSS classNames for each nav instance can be unique for custom looks
   whichMenuSet(location) {
     const loginSet = <nav className ='loginSetNavigation'>
-      <img src={voluntechLogo} className="navUILogo"/>
+      <Link to={routes.SITE_ROOT_FRONTEND}>
+      <img src={voluntechLogo} className="navUILogo"/></Link>
       <Link to={routes.LOGIN_FRONTEND} className='navLink'>Login</Link>
       <Link to={routes.SIGNUP_FRONTEND} className='navLink'>Sign Up</Link>
+      <Link to={routes.SITE_ROOT_FRONTEND} className='navLink'>About Us</Link>
+      <Link to={routes.SITE_ROOT_FRONTEND} className='navLink'>Projects</Link>
+      <Link to={routes.SITE_ROOT_FRONTEND} className='navLink'>Home</Link>
     </nav>;
 
     if (location.pathname === routes.LOGIN_FRONTEND) {
