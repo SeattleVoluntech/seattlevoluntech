3af47e70c470c4d54cb30db7820097d0b488b02f
tara-johnson <38671112+tara-johnson@users.noreply.github.com>
2019-01-08 20:23:49 -0800

    Fixing Richard's broken stuff (#28)

--- src/main/java/org/seattlevoluntech/controllers/HomeController.java
+++ src/main/java/org/seattlevoluntech/controllers/HomeController.java
@@ -2,10 +2,11 @@ package org.seattlevoluntech.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping(value = "/")
