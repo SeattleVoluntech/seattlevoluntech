650714be4f11036fb8412df651a851daafa5f3b6
jenny <yuanfang@seattleu.edu>
2019-01-12 15:06:57 -0800

    Added Skills

---
+++ src/main/java/org/seattlevoluntech/storage/SkillsRepository.java
@@ -0,0 +1,11 @@
+package org.seattlevoluntech.storage;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SkillsRepository extends CrudRepository<Skills, Long> {
+
+    //List<Skills> findBySkillName(String skill_name);
+
+}
