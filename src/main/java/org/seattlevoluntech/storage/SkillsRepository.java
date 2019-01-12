27e03c4cd6c57e2d41f318822dd67ce688270202
jenny <yuanfang@seattleu.edu>
2019-01-12 15:44:12 -0800

    Fixed SkillsRepository

--- src/main/java/org/seattlevoluntech/storage/SkillsRepository.java
+++ src/main/java/org/seattlevoluntech/storage/SkillsRepository.java
@@ -3,7 +3,9 @@ package org.seattlevoluntech.storage;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface SkillsRepository extends CrudRepository<Skills, Long> {
 
     List<Skills> findBySkillName(String skillName);
