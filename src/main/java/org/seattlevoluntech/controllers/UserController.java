3dcc9c625b78563bb8272270c5e02e440cd4e7c6
Trevor Schaff <tschaff@zulily.com>
2019-01-12 15:01:34 -0800

    cleanup and adding /user endpoint

--- src/main/java/org/seattlevoluntech/controllers/UserController.java
+++ src/main/java/org/seattlevoluntech/controllers/UserController.java
@@ -1,7 +1,8 @@
 package org.seattlevoluntech.controllers;
 
+import com.google.common.collect.Lists;
 import org.seattlevoluntech.storage.User;
-import org.seattlevoluntech.storage.UserRepository;
+import org.seattlevoluntech.storage.UsersRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RestController
 public class UserController {
 
-    @Autowired UserRepository userRepository;
+    @Autowired UsersRepository userRepository;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/user")
+    public List<User> users(final HttpServletRequest request) {
+      logger.info(request.getRemoteUser());
+
+      return Lists.newArrayList(userRepository.findAll());
+    }
+
 
     @RequestMapping("/findByLastName")
     public String fetchDataByLastName(@RequestParam("last_name") String lastName){
--- src/main/java/org/seattlevoluntech/storage/UsersRepository.java
+++ src/main/java/org/seattlevoluntech/storage/UsersRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UsersRepository extends CrudRepository<User, Long> {
   List<User> findByLastName(String lastName);
 }
