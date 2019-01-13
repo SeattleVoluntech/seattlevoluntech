076f5bb635934e1d312aca372b5c03df2856217e
Tara Johnson <tara@madface.com>
2019-01-12 16:09:44 -0800

    Create project PostMapping working.

--- src/main/java/org/seattlevoluntech/controllers/ProjectController.java
+++ src/main/java/org/seattlevoluntech/controllers/ProjectController.java
@@ -6,25 +6,37 @@ import org.seattlevoluntech.storage.ProjectsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@RestController
+@RestController(value = "/projects")
 public class ProjectController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private ProjectsRepository projectsRepository;
 
-    @RequestMapping(value = "/projects")
+    // View all projects
+    @GetMapping
     public List<Project> projects(final HttpServletRequest request) {
         logger.info(request.getRemoteUser());
 
         return Lists.newArrayList(projectsRepository.findAll());
     }
+
+    // Create project
+    @PostMapping
+    public Project createProject(@RequestBody Project project){
+        return projectsRepository.save(project);
+    }
+
+    // View single project
+//    @GetMapping("/{id}")
+//    @ResponseBody
+//    public Project
 }
