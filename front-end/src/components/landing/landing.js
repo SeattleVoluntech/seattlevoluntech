2672a228fd456783604395117f46655abfd4b812
Ming Ho <hkboi108@hotmail.com>
2019-01-14 00:56:55 -0800

    Added section to prompt visitors to sign up

--- front-end/src/components/landing/landing.js
+++ front-end/src/components/landing/landing.js
@@ -1,5 +1,6 @@
 // packages
 import React from 'react';
+import posed from 'react-pose';
 
 // custom components
 import LandingAuthForm from '../landing-auth-form/landing-auth-form'; // eslint-disable-line
@@ -19,7 +20,9 @@ import * as routes from '../../routes';
 class Landing extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      user: null,
+    };
   }
 
   signUpForm() {
@@ -36,6 +39,14 @@ class Landing extends React.Component {
     </div>;
   }
 
+  signUp = (e) => {
+    if (e.target.className === 'signup-business') {
+      this.setState({ user: 'business' });
+    } else if (e.target.className === 'signup-volunteer') {
+      this.setState({ user: 'volunteer' });
+    }
+  }
+
   render() {
     const { location } = this.props;
     const businessText = <p>Are you a small business owner who needs a website to
@@ -43,6 +54,13 @@ class Landing extends React.Component {
 
     const volunteerText = <p>Are you a tech professional/enthusiast who is passionate
         about making a difference in your local community with your skills and time?</p>;
+    const Button = posed.button({
+      hoverable: true,
+      pressable: true,
+      init: { scale: 1 },
+      hover: { scale: 1.2 },
+      press: { scale: 1.1 }
+    })
     return (
       <React.Fragment>
         <section>
@@ -51,15 +69,19 @@ class Landing extends React.Component {
             <LandingImage />
             {/* { location.pathname === routes.LOGIN_FRONTEND ? this.loginForm() : null } */}
             {/* { location.pathname === routes.SIGNUP_FRONTEND ? this.signUpForm() : null } */}
-            <div className="intro-body">
-              <div className="business">
-                { <img src={cashRegister} /> }
+            <div className='intro-body'>
+              <div className='business'>
+                <img src={cashRegister} alt='cash register icon'/>
                 { businessText }
+                <Button id='signup-business' className='signup-business'
+                  onClick={this.signUp}>Yes, I have a project!</Button>
               </div>
-              <div className="vertical-line" />
-              <div className="volunteer">
-                { <img src={volunteer} /> }
+              <div className='vertical-line' />
+              <div className='volunteer'>
+                <img src={volunteer} alt='people hand raise icon'/>
                 { volunteerText }
+                <Button id='signup-volunteer' className='signup-volunteer'
+                  onClick={this.signUp}>Yes, I want to help!</Button>
               </div>
             </div>
           </section>
