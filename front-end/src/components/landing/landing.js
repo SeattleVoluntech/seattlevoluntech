daef487e8a79f55fd4271787b58d8a32f4da9aa1
Ming Ho <hkboi108@hotmail.com>
2019-01-14 00:53:47 -0800

    Added footer component to landing page

--- front-end/src/components/landing/landing.js
+++ front-end/src/components/landing/landing.js
@@ -6,6 +6,7 @@ import LandingAuthForm from '../landing-auth-form/landing-auth-form'; // eslint-
 import NavUi from '../nav-ui/nav-ui';
 import LandingImage from './landingImage';
 import LatestProjects from './latestProjects';
+import Footer from '../footer/footer';
 import volunteer from '../../../assets/red_Volunteer.png';
 import cashRegister from '../../../assets/red_Cash.png';
 
@@ -64,6 +65,7 @@ class Landing extends React.Component {
           </section>
           <LatestProjects />
         </section>
+        <Footer />
       </React.Fragment>
     );
   }
