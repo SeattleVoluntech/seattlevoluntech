ee77ba9d1b72af0c504c767c143b7fe955d9264f
Ming Ho <hkboi108@hotmail.com>
2019-01-14 00:58:25 -0800

    Added supporting text to provide more details about the site

--- front-end/src/components/landing/landing.js
+++ front-end/src/components/landing/landing.js
@@ -26,15 +26,15 @@ class Landing extends React.Component {
   }
 
   signUpForm() {
-    return <div className="centered">
-      <h2 className="formHeader">Sign Up</h2>
+    return <div className='centered'>
+      <h2 className='formHeader'>Sign Up</h2>
       <LandingAuthForm type='signup' onComplete={this.handleSignup}/>
     </div>;
   }
 
   loginForm() {
-    return <div className="centered">
-      <h2 className="formHeader">Login</h2>
+    return <div className='centered'>
+      <h2 className='formHeader'>Login</h2>
       <LandingAuthForm type='login' onComplete={this.handleLogin}/>
     </div>;
   }
@@ -54,6 +54,15 @@ class Landing extends React.Component {
 
     const volunteerText = <p>Are you a tech professional/enthusiast who is passionate
         about making a difference in your local community with your skills and time?</p>;
+
+    const about = <div className='about-container'>
+      <p>Seattle VolunTech is a platform that connects minority and immigrant-owned
+      small businesses with technical resources necessary to improve web presence and brand
+      awareness in the Seattle area.</p>
+      <p>Seattle VolunTech is one of three projects resulting from
+      the efforts of the <a href='https://www.washingtontechnology.org/'>WTIA</a> ION Collaborators - Cohort 3 developed and maintained in concert
+      with Codefellows code school in Seattle.</p></div>;
+
     const Button = posed.button({
       hoverable: true,
       pressable: true,
@@ -84,6 +93,9 @@ class Landing extends React.Component {
                   onClick={this.signUp}>Yes, I want to help!</Button>
               </div>
             </div>
+          </section>
+          <section className ='main-details'>
+            { about }
           </section>
           <LatestProjects />
         </section>
