9a59d6a448163f1fb68ac63ab34c7d2135901d82
Tara Johnson <tara@madface.com>
2019-01-15 22:01:43 -0800

    Moved error controller and moved error model.

--- src/main/java/org/seattlevoluntech/controllers/ErrorController.java
+++ src/main/java/org/seattlevoluntech/controllers/ErrorController.java
@@ -2,14 +2,9 @@ package org.seattlevoluntech.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.io.IOException;
 
 @SuppressWarnings("unused")
 @Controller
