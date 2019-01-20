b49a012d5c389a73e8830c9727d74c62f64d8826
jenny <yuanfang@seattleu.edu>
2019-01-20 12:55:30 -0800

    Created Endpoint to return currently logged user

--- src/main/java/org/seattlevoluntech/controllers/UserController.java
+++ src/main/java/org/seattlevoluntech/controllers/UserController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -26,6 +29,11 @@ public class UserController {
       return Lists.newArrayList(userRepository.findAll());
     }
 
+    @RequestMapping(value = "/userId", method = RequestMethod.GET)
+    @ResponseBody
+    public String currentUserId(Authentication authentication) {
+        return authentication.getName();
+    }
 
     @RequestMapping("/findByLastName")
     public String fetchDataByLastName(@RequestParam("last_name") String lastName){
