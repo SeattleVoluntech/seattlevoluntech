4deaf24f0f74a4aa1845ea22ec53dc0ea9b53f1b
Ming Ho <hkboi108@hotmail.com>
2019-01-24 20:15:59 -0800

    Added profile component

---
+++ front-end/src/components/dashboard/profile.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import ProjectCard from '../open-projects/project-card';
+
+// styles
+import './profile.scss';
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { location } = this.props;
+    console.log(this.props);
+
+    const businessProfile = <div className='profile-info flex'><div className='profile-container'><h3>Business Name</h3>
+    <span><h4>Business Email : </h4><p>this.props.business.email</p></span>
+    <span><h4>Business Description: </h4><p>this.props.business.description</p></span>
+    <span><h4>Business Website: </h4><p>this.props.business.website</p></span>
+    </div>
+    </div>;
+
+    const volunteerProfile = <div><h3>Volunteer Name</h3><h3>Volunteer Email</h3></div>;
+
+    return (
+        <React.Fragment>
+          {this.props.user === 'business' && (
+              <React.Fragment>
+              {businessProfile}
+              <div className='profile-projects flex'>
+                <button>Create a new project</button>
+                <div className='open-projects flex'>
+                  <h3>Open Projects</h3>
+                  <ProjectCard />
+                </div>
+                <hr className='underline' />
+                <div className='closed-projects flex'>
+                  <h3>Closed Projects</h3>
+                  <ProjectCard />
+                </div>
+              </div>)
+          </React.Fragment>
+          )}
+          {this.props.user === 'volunteer' && (
+            <React.Fragment>
+              <div className='profile'>
+                {volunteerProfile}
+                <div className='current-projects flex'>
+                  <h3>Current Projects</h3>
+                  <ProjectCard />
+                </div>
+                <hr className='underline' />
+                <h3>Past Projects</h3>
+                <div className='closed-projects flex'>
+                  <ProjectCard />
+                </div>
+              </div>
+            </React.Fragment>
+          )}
+        </React.Fragment>
+    );
+  }
+}
+
+export default Profile;
