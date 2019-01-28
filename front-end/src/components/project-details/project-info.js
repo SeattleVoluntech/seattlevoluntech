9723b046803b241ac07f09e4a5290e8c2f8081a5
Ming Ho <hkboi108@hotmail.com>
2019-01-27 18:34:51 -0800

    Added sign up and edit project buttons to project info component

--- front-end/src/components/project-details/project-info.js
+++ front-end/src/components/project-details/project-info.js
@@ -1,25 +1,41 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Link from 'react-router';
 import './project-info.scss';
+
+const SignUpButton = (props) => {
+  return (
+    <Link to='/thank-you'><button >I want to work on this!</button></Link>
+  );
+};
+
+const EditProjectButton = (props) => {
+  return (
+    <Link to='/project-edit'><button>Edit this project</button></Link>
+  );
+};
 
 const ProjectInfo = (props) => {
   return (
     <div className='project-info flex'>
-      <div className='project-container'>
+      <div className='project-wrapper'>
         <h2 className='project-name'>{props.projectName || 'Project Name'}</h2>
         <hr className='header-underline'/>
         <div className='project-desc'>
           <h4>Project Description:</h4>
-          <p>{props.projectDesc || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elit massa, lobortis at augue sit amet, faucibus pellentesque ante. Nunc sit amet euismod mauris, eu ornare eros. Quisque et quam ultricies, pretium enim eu, iaculis leo. Ut pellentesque mattis orci vel porta. '}</p>
+          <p>{props.projectDesc || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elit massa, lobortis at augue sit amet, faucibus pellentesque ante. Nunc sit amet euismod mauris, eu ornare eros. Quisque et quam ultricies, pretium enim eu, iaculis leo. Ut pellentesque mattis orci vel porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elit massa, lobortis at augue sit amet, faucibus pellentesque ante. Nunc sit amet euismod mauris, eu ornare eros. Quisque et quam ultricies, pretium enim eu, iaculis leo. Ut pellentesque mattis orci vel porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elit massa, lobortis at augue sit amet, faucibus pellentesque ante. Nunc sit amet euismod mauris, eu ornare eros. Quisque et quam ultricies, pretium enim eu, iaculis leo. Ut pellentesque mattis orci vel porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elit massa, lobortis at augue sit amet, faucibus pellentesque ante. Nunc'}</p>
         </div>
-        {props.isBusiness ? <button>edit</button> : null}
+        <div className='project-buttons flex'>
+          {props.isBusiness ? <SignUpButton /> : null}
+          {props.isVolunteer ? <EditProjectButton /> : null}
+        </div>
       </div>
-      <div className='business-container'>
+      <div className='business-wrapper'>
         <h2 className='business-name'>{props.businessName || 'Business Name'}</h2>
         <hr className='header-underline'/>
         <div className='business-desc'>
           <h4>Business Description:</h4>
           <p>{props.businessDesc || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elit massa, lobortis at augue sit amet, faucibus pellentesque ante. Nunc sit amet euismod mauris, eu ornare eros. Quisque et quam ultricies, pretium enim eu, iaculis leo. Ut pellentesque mattis orci vel porta. '}</p>
-          <a href={props.businessLink} className='existing-site' rel="noopener noreferrer" target="_blank">{props.businessName || 'Link to existing site'}</a>
+          <a href={props.businessSite} className='existing-site' rel="noopener noreferrer" target="_blank">{props.businessName || 'Link to existing site'}</a>
         </div>
       </div>
     </div>
