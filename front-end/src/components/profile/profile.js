47ab9e5a3c29f3affb1342f82d3fe5b8783792a6
Ming Ho <hkboi108@hotmail.com>
2019-01-28 11:41:30 -0800

    Added links to edit project and create project buttons

--- front-end/src/components/profile/profile.js
+++ front-end/src/components/profile/profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import ProjectCard from '../open-projects/project-card';
 
@@ -13,53 +14,65 @@ class Profile extends React.Component {
   render() {
     const { location } = this.props;
 
-    const businessProfile = <div className='profile-info flex'><div className='profile-container'><h3>this.props.businessname</h3>
-    <span><h4>Business Email : </h4><p>this.props.business.email</p></span>
-    <span><h4>Business Description: </h4><p>this.props.business.description</p></span>
-    <span><h4>Business Website: </h4><p>this.props.business.website</p></span>
-    </div>
-    </div>;
+    const businessProfile = <div className='profile-info flex'><div className='profile-container flex'><h3>this.props.businessname</h3>
+      <span><h4>Business Email : </h4><p>this.props.business.email</p></span>
+      <span><h4>Business Description: </h4><p>this.props.business.description</p></span>
+      <span><h4>Business Website: </h4><p>this.props.business.website</p></span>
+      <div className='business-profile-buttons'>
+      <Link to='dashboard-edit'><button>Edit your profile</button></Link>
+      <Link to='/project-new'><button>Create a new project</button></Link>
+      </div>
+      </div></div>;
 
-    const volunteerProfile = <div><h3>Volunteer Name</h3><h3>Volunteer Email</h3></div>;
+    const volunteerProfile = <div className='profile-info flex'><div className='profile-container flex'><h3>this.props.volunteername</h3>
+      <span><h4>Volunteer Email :</h4><p>this.props.volunteer.email</p></span>
+      <span><h4>Volunteer Skills :</h4><p>this.props.volunteer.skills</p></span>
+      <Link to='/dashboard-edit'><button>Edit your profile</button></Link>
+      </div></div>;
 
     return (
         <React.Fragment>
           {this.props.user === 'business' && (
-              <div className='profile'>
-              {businessProfile}
-              <div className='profile-projects flex'>
-                <button>Create a new project</button>
-                <div className='open-projects flex'>
-                  <h3>Open Projects</h3>
-                  <ProjectCard />
-                  <ProjectCard />
-                  <ProjectCard />
-                  <ProjectCard />
-                </div>
-                <hr className='underline' />
-                <div className='closed-projects flex'>
-                  <h3>Closed Projects</h3>
-                  <ProjectCard />
-                </div>
+            <div className='profile'>
+            {businessProfile}
+            <div className='profile-projects flex'>
+              <div className='open-projects flex'>
+                <h2>Open Projects</h2>
+                {/* map over project object to generate cards and pass userType as props */}
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
               </div>
+              <hr className='underline' />
+              <div className='closed-projects flex'>
+                <h2>Closed Projects</h2>
+                {/* map over project object to generate cards and pass userType as props */}
+                <ProjectCard />
+              </div>
+            </div>
             </div>)
           }
           {this.props.user === 'volunteer' && (
             <React.Fragment>
               <div className='profile'>
                 {volunteerProfile}
-                <div className='current-projects flex'>
-                  <h3>Current Projects</h3>
-                  <ProjectCard />
-                </div>
-                <hr className='underline' />
-                <div className='closed-projects flex'>
-                  <h3>Past Projects</h3>
-                  <ProjectCard />
+                <div className='profile-projects flex'>
+                  <div className='current-projects flex'>
+                    <h2>Current Projects</h2>
+                    {/* map over project object to generate cards and pass userType as props */}
+                    <ProjectCard />
+                  </div>
+                  <hr className='underline' />
+                  <div className='closed-projects flex'>
+                    <h2>Past Projects</h2>
+                    {/* map over project object to generate cards and pass userType as props */}
+                    <ProjectCard />
+                  </div>
                 </div>
               </div>
             </React.Fragment>
-          )}    
+          )}
         </React.Fragment>
     );
   }
