ae8b35d38aa146feb3e2b5129690cecc781b09eb
Ming Ho <hkboi108@hotmail.com>
2019-01-29 20:45:53 -0800

    Connecting latest projects endpoint to home page latest projects. #83

--- front-end/src/components/open-projects/project-card.js
+++ front-end/src/components/open-projects/project-card.js
@@ -4,13 +4,14 @@ import * as routes from '../../routes';
 import './project-card.scss';
 
 const ProjectCard = (props) => {
+  console.log(props);
   return (
     <div className="aProject">
       { /* <div className="card-top"> */ }
         <Link to={routes.PROJECT_DETAILS_FRONTEND} >
-          <h2>Project Name</h2>
-          <h3>Project Sponsor</h3>
-          <h4>Project Headline</h4>
+          <h2>{props.currentProject.projectName}</h2>
+          <h3>{props.currentProject.businessName}</h3>
+          <h4>this.props.projectheadline</h4>
         </Link>
       {/*</div>*/}
     </div>
